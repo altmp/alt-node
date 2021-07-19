389f359f110af4d17679dd85dce4a169c95496ef
LeonMrBonnie <leonmrbonnie@hotmail.com>
2021-07-20 01:40:34 +0200

    Add support for shared module

--- alt-build/alt-debug.bat
+++ alt-build/alt-debug.bat
@@ -1,3 +1,3 @@
 cd ..
-call vcbuild x64 debug dll vs2019 no-cctest link-module ./alt-build/alt.js link-module ./alt-build/alt-server.js
+call vcbuild x64 debug dll vs2019 no-cctest link-module ./alt-build/alt.js link-module ./alt-build/alt-server.js link-module ./alt-build/alt-shared.js
 cd alt-build
--- alt-build/alt-release.sh
+++ alt-build/alt-release.sh
@@ -1,4 +1,4 @@
 cd ..
-./configure --shared --link-module ./alt-build/alt.js --link-module ./alt-build/alt-server.js
+./configure --shared --link-module ./alt-build/alt.js --link-module ./alt-build/alt-server.js --link-module ./alt-build/alt-shared.js
 sudo make -j4
 cd alt-build
