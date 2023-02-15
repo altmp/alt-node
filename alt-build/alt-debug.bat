fdba742190e2ba1de738a098f0cd513ba35a3c11
LeonMrBonnie <leonmrbonnie@hotmail.com>
2023-02-15 16:28:25 +0100

    Fix missing config.h for Windows

--- alt-build/alt-debug.bat
+++ alt-build/alt-debug.bat
@@ -1,3 +1,4 @@
 cd ..
+echo.> deps/base64/base64/lib/config.h
 call vcbuild x64 debug dll vs2019 no-cctest
 cd alt-build
--- alt-build/alt-release.bat
+++ alt-build/alt-release.bat
@@ -1,3 +1,4 @@
 cd ..
+echo.> deps/base64/base64/lib/config.h
 call vcbuild release x64 dll vs2019 no-cctest
 cd alt-build
